9c67824239793e292d7e42433bc9a96eb366412d
mclark <maryariley4578@yahoo.com>
2023-11-28 17:50:21 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+//Example fetch using pokemonapi.co
+document.querySelector("button").addEventListener("click", getFetch);
+
+function getFetch() {
+  const choice = document.querySelector("input").value;
+  const url = `https://api.nasa.gov/planetary/apod?api_key=32WoR5n1XFZtvrVyRPRMLzCA97MWp8wQW6UdzSFt&date=${choice}
+  `;
+
+  fetch(url)
+    .then((res) => res.json()) // parse response as JSON
+    .then((data) => {
+      console.log(data);
+      //checks if pic of day is image or video so video can display correctly
+      if (data.media_type === "image") {
+        document.querySelector("img").src = data.hdurl;
+      } else if (data.media_type === "video") {
+        document.querySelector("iframe").src = data.url;
+      }
+
+      document.querySelector("h3").innerText = data.explanation;
+    })
+    .catch((err) => {
+      console.log(`error ${err}`);
+    });
+}
+
+// use postman to test api
+// get nasa key log in with account
+//  https://api.nasa.gov/planetary/apod?api_key=32WoR5n1XFZtvrVyRPRMLzCA97MWp8wQW6UdzSFt
+//homework to do
+// ****hiding and showing of image if video part of a date
+//do something different and make it your own how to make it look better
